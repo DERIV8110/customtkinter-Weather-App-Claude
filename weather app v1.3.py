62a2a7c3201be6dd955050bc5c608d596de35778
Abhilash Ray <abhilashrai9896@gmail.com>
2026-06-16 03:17:47 +0530

    Add files via upload
    IP auto detected when launching app and data fetched. Manual search is also available for any place.

---
+++ weather app v1.3.py
@@ -0,0 +1,108 @@
+import customtkinter
+from tkinter import *
+import ttkbootstrap as ttk
+import json                            #only for pretty printing
+import requests
+from datetime import datetime
+from tkinter import messagebox
+
+home = customtkinter.CTk()
+home.geometry("500x300")
+
+frame = ttk.Frame(home)
+frame.grid(row=0, column=0)
+home.grid_rowconfigure(0, weight=2)
+home.grid_rowconfigure(1, weight=2)
+home.grid_rowconfigure(2, weight=1)
+home.grid_rowconfigure(3, weight=1)
+home.grid_rowconfigure(4, weight=1)
+
+home.grid_columnconfigure(0, weight=1)
+home.grid_columnconfigure(1, weight=1)
+home.grid_columnconfigure(2, weight=1)
+home.grid_columnconfigure(3, weight=1)
+
+now = datetime.now()
+
+date_label = customtkinter.CTkLabel(home,text=now.strftime("%d %B %Y"))
+date_label.grid(row=0,column=0)
+
+day_label = customtkinter.CTkLabel(home,text=now.strftime("%A"))
+day_label.grid(row=0,column=3)
+
+search_label = customtkinter.CTkLabel(home,text="Enter Location")
+search_label.grid(row=1,column=0)
+
+entry = customtkinter.CTkEntry(home, placeholder_text="First letter Uppercase",width=150)
+entry.grid(row=1,column=3)
+
+ip = f"http://ip-api.com/json/"
+response3 = requests.get(ip)
+sdata3 = response3.json()
+latitude2 = sdata3['lat']
+longitude2 = sdata3['lon']
+#print("temp data:",sdata3)
+
+ip_lat_lon = f"https://api.open-meteo.com/v1/forecast?latitude={latitude2}&longitude={longitude2}&hourly=temperature_2m"
+response4 = requests.get(ip_lat_lon)
+sdata4 = response4.json()
+#print("temp data:",sdata2)
+
+Name_city = customtkinter.CTkLabel(home,text=sdata3['city'])
+Name_city.grid(row=2,column=0)
+
+temp_data2 = sdata4["hourly"]["temperature_2m"]
+min_temp_label2 = customtkinter.CTkLabel(home,text=min(temp_data2))
+min_temp_label2.grid(row=2,column=3)                                                # f" with {} in a continous string separates variables
+                                                                      #and string
+max_temp_label2 = customtkinter.CTkLabel(home,text=max(temp_data2))
+max_temp_label2.grid(row=3,column=3)
+
+def fetch_data():
+       global min_temp_label2
+       global max_temp_label2
+       global Name_city
+
+       del min_temp_label2
+       del max_temp_label2
+       del Name_city
+       get_location = entry.get()
+       if get_location == "":
+              messagebox.showerror("Error","Please enter a valid region")
+       else:
+              url_geocoding = f"https://geocoding-api.open-meteo.com/v1/search?name={get_location}&count=10&language=en&format=json"
+              response1 = requests.get(url_geocoding)
+              sdata1 = response1.json()
+              #print("city data:",sdata1["results"][0])
+              latitude = sdata1["results"][0]["latitude"]
+              longitude = sdata1["results"][0]["longitude"]
+
+
+              #url consists of url of website from documentation on website then symbol and token(api key)
+              url_lat_lon = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m"
+              response2 = requests.get(url_lat_lon)
+              sdata2 = response2.json()
+              #print("temp data:",sdata2)
+
+              show_loc_label = customtkinter.CTkLabel(home,text=get_location)
+              show_loc_label.grid(row=2,column=0)
+
+              temp_data = sdata2["hourly"]["temperature_2m"]
+
+              min_temp_label = customtkinter.CTkLabel(home,text=min(temp_data))
+              min_temp_label.grid(row=2,column=3)                                                # f" with {} in a continous string separates variables
+                                                                                    #and string
+              max_temp_label = customtkinter.CTkLabel(home,text=max(temp_data))
+              max_temp_label.grid(row=3,column=3) 
+                                                                                    #f"{sdata['c']}"
+              
+              #print("Status code:", response.status_code)     # add this
+              #print("Full response:", sdata2)                 # already have this
+
+#ip_butt = customtkinter.CTkButton(home, text="Get IP", command=fetch_ip)
+#ip_butt.grid(row=4,column=1)
+
+search_butt = customtkinter.CTkButton(home, text="Search", command=fetch_data)
+search_butt.grid(row=4,column=2)
+
+home.mainloop()
